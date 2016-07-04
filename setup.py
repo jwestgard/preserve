2d239fb91850efe92e78c3ce63f71c97a023b428
jwestgard <jwestgard@mac.com>
2016-07-03 20:34:22 -0400

    cleanup files and fix setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import find_packages, setup
 setup(
     name='preserve',
     version='0.1',
-    description='Command-line preservation utilities.',
+    description='Command-line digital preservation utilities.',
     author='Joshua A. Westgard',
     author_email="westgard@umd.edu",
     platforms=["any"],
     license="BSD",
     url="http://github.com/jwestgard/preserve",
     packages=find_packages(),
-    py_modules=['preserve'],
+    scripts=['bin/preserve'],
     install_requires=[i.strip() for i in open("requirements.txt").readlines()]
 )
