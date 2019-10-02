b58a97592372a06dc2f2d4b9422b2e08bc96a70e
Joshua Westgard <jwestgard@mac.com>
2019-10-01 22:25:30 -0400

    batch mode supports blacklisting dirs

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     packages=find_packages(),
     entry_points = {
         'console_scripts': ['preserve=preserve.__main__:main',
-                            'preserve.batch=preserve.batch:main']
+                            'preserve.batch=preserve.batch.__main__:main']
         },
     install_requires=[i.strip() for i in open("requirements.txt").readlines()]
 )
