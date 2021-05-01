feb3ef96cfe5f2381dd8ab3ae133a36349f3a144
Joshua Westgard <jwestgard@mac.com>
2021-04-30 22:13:21 -0400

    add bag and partition scripts

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='preserve',
-    version='0.5',
+    version='0.6',
     description='Command-line digital preservation utilities.',
     author='Joshua A. Westgard',
     author_email="westgard@umd.edu",
@@ -12,7 +12,9 @@ setup(
     packages=find_packages(),
     entry_points = {
         'console_scripts': ['preserve=preserve.__main__:main',
-                            'preserve.batch=preserve.batch.__main__:main']
+                            'preserve.batch=preserve.batch.__main__:main',
+                            'partition=partition.__main__:main',
+                            'bag=bag.__main__:main']
         },
     install_requires=[i.strip() for i in open("requirements.txt").readlines()]
 )
