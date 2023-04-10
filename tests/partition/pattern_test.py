bc86835487439b84e0b3c637ef1917e9755c11fd
Marc Andreu Grillo Aguilar <aguilarm@umd.edu>
2023-04-10 12:16:58 -0400

    LIBITD-2238: Adding one more test string
    https://umd-dit.atlassian.net/browse/LIBITD-2238

--- tests/partition/pattern_test.py
+++ tests/partition/pattern_test.py
@@ -15,12 +15,17 @@ def test_pattern():
     # File with a bunch of extensions
     file3 = 'blah-123456-1234.a.bunch.of.file.extensions.txt'
 
+    # File with numbers after underscore
+    file4 = 'scpa-081986_1234.xml'
+
     # The old pattern shouldn't match these
     assert re.match(old_pattern, file1) is None
     assert re.match(old_pattern, file2) is None
     assert re.match(old_pattern, file3) is None
+    assert re.match(old_pattern, file4) is None
 
     # The new pattern should
     assert re.match(new_pattern, file1) is not None
     assert re.match(new_pattern, file2) is not None
     assert re.match(new_pattern, file3) is not None
+    assert re.match(new_pattern, file4) is not None
